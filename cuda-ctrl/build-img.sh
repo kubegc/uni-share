cf33da1a341b7f1b8835b60a7ef4411e03f25646
fish <skillfulrookie@163.com>
2023-11-20 13:56:11 +0800

    buildx

--- cuda-ctrl/build-img.sh
+++ cuda-ctrl/build-img.sh
@@ -6,6 +6,9 @@ set -o xtrace
 
 ROOT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd -P)
 IMAGE_FILE=${IMAGE_FILE:-"fish/vcuda:latest"}
+img="unishare/cudactl"
+version="1.0.0"
+repo="g-ubjg5602-docker.pkg.coding.net/iscas-system/containers"
 
 function cleanup() {
     rm -rf ${ROOT}/cuda-control.tar
@@ -24,7 +27,14 @@ function build_img() {
     cp ${ROOT}/Dockerfile ${ROOT}/build
     (
       cd ${ROOT}/build
-      docker build ${BUILD_FLAGS:-} --build-arg version=${version} --build-arg commit=${commit} -t ${IMAGE_FILE} .
+      #docker buildx create --name mybuilder --driver docker-container
+      #docker buildx use mybuilder --node mybuilder
+      #docker run --privileged --rm tonistiigi/binfmt --install all
+      #docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+      #docker build ${BUILD_FLAGS:-} --build-arg version=${version} --build-arg commit=${commit} -t ${IMAGE_FILE} .
+      docker buildx build /home/onceas/wyy/unishare/cuda-ctrl/build --platform linux/arm64,linux/amd64 ${BUILD_FLAGS:-} --build-arg version=${version} --build-arg commit=${commit} -t $repo/$img:v$version --push  -f /home/onceas/wyy/unishare/cuda-ctrl/build/Dockerfile
+      #docker buildx build --load --no-cache /home/onceas/wyy/gpudeploy/vcuda-controller/build    --platform linux/amd64 ${BUILD_FLAGS:-} --build-arg version=${version} --build-arg commit=${commit} -t ${IMAGE_FILE}-amd64  -f /home/onceas/wyy/gpudeploy/vcuda-controller/build/Dockerfile
+
     )
 }
 
